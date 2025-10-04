180e4ce12bbcc747ba41881644d833068f1a3242
Crlss123 <camadortjr@gmail.com>
2025-10-03 23:05:51 -0700

    fix: typo

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from models import db
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mensajes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
+db.init_app(app)

712f4799bd0ae054bed81e9ec9c7ce70b9b2dbfa
TulKasTer <23609872+TulKasTer@users.noreply.github.com>
2025-10-03 23:19:38 -0700

    feat: define the User model

---
+++ models/UserModel.py
@@ -0,0 +1,11 @@
+from models import db
+
+# Define the User model
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+    ubication = db.Column(db.String(256), nullable=False)
